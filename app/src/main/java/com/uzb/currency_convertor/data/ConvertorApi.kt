19350f04994b43683aef1764c70296146a04fd71
khurshiddev <umaribnxattob8008@gmail.com>
2025-02-07 15:31:54 +0900

    feat: add ConvertorApi.kt

--- app/src/main/java/com/uzb/currency_convertor/data/ConvertorApi.kt
+++ app/src/main/java/com/uzb/currency_convertor/data/ConvertorApi.kt
@@ -1,8 +1,16 @@
 package com.uzb.currency_convertor.data
 
+import com.uzb.currency_convertor.data.models.ExchangeResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ConvertorApi {
-    @GET
+    @GET("convert")
+    suspend fun converRate(
+        @Query("from") from:String,
+        @Query("to") to:String,
+        @Query("amount") amount:String
+    ):Response<ExchangeResponse>
 
 }
