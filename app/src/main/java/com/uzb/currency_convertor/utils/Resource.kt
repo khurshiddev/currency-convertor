85ea9af3feff43fa6d98b885ecf5d2ffaa56b0b5
khurshiddev <umaribnxattob8008@gmail.com>
2025-02-07 15:32:27 +0900

    feat: add Resource.kt

---
+++ app/src/main/java/com/uzb/currency_convertor/utils/Resource.kt
@@ -0,0 +1,7 @@
+package com.uzb.currency_convertor.utils
+
+sealed class Resource<T>(data: T?, message: String? = null) {
+
+    data class Success<T>(val data: T?): Resource<T>(data,null)
+    data class Error<T>(val message: String?):Resource<T>(null,message)
+}
