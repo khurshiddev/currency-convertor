b90ada513456b36d7a9a8837421434750d8e8eba
khurshiddev <umaribnxattob8008@gmail.com>
2025-02-15 19:14:53 +0900

    refactor: MainActivity.kt

--- app/src/main/java/com/uzb/currency_convertor/MainActivity.kt
+++ app/src/main/java/com/uzb/currency_convertor/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.uzb.currency_convertor
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.lifecycle.lifecycleScope
+import com.uzb.currency_convertor.main.MainViewModel
+import com.uzb.currency_convertor.utils.ConvertEvent
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val TAG = "TAG"
+    private val viewModel: MainViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        viewModel.getConvertRate("USD", "KRW", "20")
+        lifecycleScope.launchWhenCreated {
+            viewModel.conversion.collectLatest {
+                when (it) {
+                    ConvertEvent.Empty -> Unit
+                    is ConvertEvent.Error -> Log.d(TAG, "onCreate:  ${it.errorMessage}")
+                    ConvertEvent.Loading -> Unit
+                    is ConvertEvent.Success -> Log.d(TAG, "onCreate: ${it.result}")
+                }
+            }
         }
     }
 }
