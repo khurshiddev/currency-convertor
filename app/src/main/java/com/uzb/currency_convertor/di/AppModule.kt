603332290f098d50b7f97d282d93e799fdcecc05
khurshiddev <umaribnxattob8008@gmail.com>
2025-02-15 19:14:09 +0900

    refactor: AppModule.kt

--- app/src/main/java/com/uzb/currency_convertor/di/AppModule.kt
+++ app/src/main/java/com/uzb/currency_convertor/di/AppModule.kt
@@ -1,11 +1,14 @@
 package com.uzb.currency_convertor.di
 
 import com.uzb.currency_convertor.data.ConvertorApi
+import com.uzb.currency_convertor.main.MainRepository
+import com.uzb.currency_convertor.main.MainRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
@@ -18,6 +21,14 @@ object AppModule {
     fun getConverterApi():ConvertorApi {
         return  Retrofit
             .Builder()
-            .build().create(ConvertorApi::class.java)
+            .baseUrl("https://api.apilayer.com/currency_data/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ConvertorApi::class.java)
     }
+
+
+    @Singleton
+    @Provides
+    fun getMainRepository(api:ConvertorApi):MainRepository = MainRepositoryImpl(api)
 }
